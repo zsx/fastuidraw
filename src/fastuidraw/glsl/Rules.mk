2335547b452b1a1ac55fb88b7391b120c277515e
Kevin Rogovin <kevin.rogovin@intel.com>
2016-10-26 10:43:57 +0300

    fastuidraw/painter, glsl: define PainterBrushShader

--- src/fastuidraw/glsl/Rules.mk
+++ src/fastuidraw/glsl/Rules.mk
@@ -13,7 +13,7 @@ include $(dir)/Rules.mk
 
 LIBRARY_SOURCES += $(call filelist, shader_source.cpp shader_code.cpp \
 	painter_item_shader_glsl.cpp painter_blend_shader_glsl.cpp \
-	painter_backend_glsl.cpp)
+	painter_brush_shader_glsl.cpp painter_backend_glsl.cpp)
 
 
 # Begin standard footer
