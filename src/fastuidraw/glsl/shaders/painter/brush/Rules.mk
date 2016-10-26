5e11d3000e0a7e6b7bfd849d11fba9c9c6282a80
Kevin Rogovin <kevin.rogovin@intel.com>
2016-10-26 13:16:07 +0300

    fastuidraw/glsl/shaders/painter/brush: move current brush shaders to new directory, fixed_function

--- src/fastuidraw/glsl/shaders/painter/brush/Rules.mk
+++ src/fastuidraw/glsl/shaders/painter/brush/Rules.mk
@@ -4,13 +4,8 @@ dirstack_$(sp)	:= $(d)
 d		:= $(dir)
 # End standard header
 
-LIBRARY_RESOURCE_STRING += $(call filelist, fastuidraw_painter_brush.vert.glsl.resource_string \
-	fastuidraw_painter_brush_types.glsl.resource_string \
-	fastuidraw_painter_brush_unpack.glsl.resource_string \
-	fastuidraw_painter_brush_unpack_forward_declares.glsl.resource_string \
-	fastuidraw_painter_brush_macros.glsl.resource_string \
-	fastuidraw_painter_brush.frag.glsl.resource_string \
-	)
+dir := $(d)/fixed_function
+include $(dir)/Rules.mk
 
 # Begin standard footer
 d		:= $(dirstack_$(sp))
